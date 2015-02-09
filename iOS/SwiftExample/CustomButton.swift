90d128504c8346630364a2f42f82ebcb8a4b1ca9
Siddesh Pillai <siddeshvijji@gmail.com>
2015-02-08 19:10:13 -0500

    Custom Buttons Updated

--- iOS/SwiftExample/CustomButton.swift
+++ iOS/SwiftExample/CustomButton.swift
@@ -8,6 +8,8 @@ class MyCustomButton: UIButton {
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-     
+        self.layer.cornerRadius = 5.0;
+        self.layer.borderColor = UIColor.redColor().CGColor
+        self.tintColor = UIColor.whiteColor()
     }
 }
